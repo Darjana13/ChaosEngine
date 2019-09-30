49bb5df744d54ea290bd2830f06c8018948012f7
NIKITA\Никита <khandei@mail.ru>
2019-10-01 03:35:03 +0700

    Set up project, added OpenTK, added references, created OpenGL bases

---
+++ ChaosEngine/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+
+namespace ChaosEngine
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowsFormsHost_Initialized(object sender, EventArgs e)
+        {
+            GLView.MakeCurrent();
+        }
+
+        private void GLView_Load(object sender, EventArgs e)
+        {
+            GL.ClearColor(Color.FromArgb(100, 100, 100));
+        }
+
+        private void GLView_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            GL.Viewport(0, 0, GLView.Width, GLView.Height);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GLView.SwapBuffers();
+        }
+    }
+}
